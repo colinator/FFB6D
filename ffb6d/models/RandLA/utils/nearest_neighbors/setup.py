40a3fd8753a614ba23132bd9fe9e38650a3c70d8
Colin Prepscius <colinprepscius@gmail.com>
2022-08-02 16:11:19 -0400

    Update setup.py

--- ffb6d/models/RandLA/utils/nearest_neighbors/setup.py
+++ ffb6d/models/RandLA/utils/nearest_neighbors/setup.py
@@ -3,6 +3,11 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
+def buildit():
+    from Cython.Distutils import build_ext
+    return build_ext
+
+
 ext_module = Extension(
     "nearest_neighbors",
     sources=["knn.pyx", "knn_.cxx",],  # source file(s)
@@ -17,7 +22,7 @@ setup(
     ext_modules = [ext_module],
     cmdclass = {'build_ext': build_ext},
     install_requires=[
-        'Cython',
+        'cython',
         'numpy',
     ],
 )
