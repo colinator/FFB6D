64fcb19ec11cdd4d8552ffa857b7b5e7fbef88c4
Colin Prepscius <colinprepscius@gmail.com>
2022-08-02 16:23:19 -0400

    Update setup.py

--- ffb6d/models/RandLA/utils/nearest_neighbors/setup.py
+++ ffb6d/models/RandLA/utils/nearest_neighbors/setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup
 from distutils.extension import Extension
-#from Cython.Distutils import build_ext
-#import numpy
+from Cython.Distutils import build_ext
+import numpy
 
-def buildit():
-    from Cython.Distutils import build_ext
-    return build_ext
+#def buildit():
+#    from Cython.Distutils import build_ext
+#    return build_ext
 
-def numpy_includes():
-    import numpy as np
-    return np.get_include()
+#def numpy_includes():
+#    import numpy as np
+#    return np.get_include()
 
 
 ext_module = Extension(
     "nearest_neighbors",
     sources=["knn.pyx", "knn_.cxx",],  # source file(s)
-    include_dirs=["./", numpy_includes()], #numpy.get_include()],
+    include_dirs=["./", numpy.get_include()],
     language="c++",            
     extra_compile_args=["-std=c++11", "-fopenmp"],
     extra_link_args=["-std=c++11", '-fopenmp'],
@@ -24,7 +24,7 @@ ext_module = Extension(
 setup(
     name = "KNNNanoFLANN",
     ext_modules = [ext_module],
-    cmdclass = {'build_ext': buildit},
+    cmdclass = {'build_ext': build_ext},
     install_requires=[
         'Cython',
         'numpy',
